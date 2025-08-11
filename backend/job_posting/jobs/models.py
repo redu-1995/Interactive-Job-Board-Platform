eed94ed4145267ab317ecdc3d96b1d075655a3ff
Rediet Abreham <nara1415karol@gmail.com>
2025-08-11 20:28:53 +0300

    feat: add Job model and migrations

--- backend/job_posting/jobs/models.py
+++ backend/job_posting/jobs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    EXPERIENCE_LEVELS = [
+        ('ENTRY', 'Entry-Level'),
+        ('MID', 'Mid-Level'),
+        ('SENIOR', 'Senior'),
+    ]
+    
+    title = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    experience_level = models.CharField(max_length=20, choices=EXPERIENCE_LEVELS)
+    description = models.TextField()
+    application_link = models.URLField()
+    posted_at = models.DateTimeField(auto_now_add=True)
